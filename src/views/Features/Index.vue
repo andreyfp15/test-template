1b7bfb2447ed6159aa2ea467a3ced33f1565b8be
Arthur Brites <arth.brites@gmail.com>
2024-05-02 11:26:57 -0300

    feat: added delete button

--- src/views/Features/Index.vue
+++ src/views/Features/Index.vue
@@ -2,7 +2,6 @@
 import { defineComponent, ref } from 'vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import TitlePageDefault from '@/components/Titles/TitlePageDefault.vue'
-
 </script>
 
 <script lang="ts">
@@ -17,7 +16,6 @@ import Column from 'primevue/column';
 import InputText from 'primevue/inputtext';
 import { FilterMatchMode } from 'primevue/api';
 import Dropdown from 'primevue/dropdown';
-import ColumnGroup from 'primevue/columngroup';
 
 export default defineComponent({
   components: {},
@@ -27,11 +25,11 @@ export default defineComponent({
       features: [] as Feature[],
       loading: ref(true),
       products: [{
-        key: "1a",
+        key: "1",
         value: 'WMS'
       },
       {
-        key: "2s",
+        key: "2",
         value: 'CRM'
       }
     ] as Option[],
@@ -53,16 +51,13 @@ export default defineComponent({
     this.loading = false;
   },
   methods: {
-    deleteRow(props: any) {
-      delete this.features[props.id]
-      console.log("WOW")
+    deleteRow(id: any) {
+      this.features = this.features.filter(i => i.id != id);
     },
     onRowEditSave(event: any) {
-            console.log(this.products);
-            let { newData, index } = event;
-
-            this.features[index] = newData;
-        }
+          let { newData, index } = event;
+          this.features[index] = newData;
+    }
   }
 });
 </script>
@@ -130,8 +125,8 @@ export default defineComponent({
         </Column>
         <Column header="Ações" :rowEditor="true" style="width: 5%; min-width: 8rem" />
         <Column style="width: 5%; min-width: 8rem">
-          <template #body>
-            <button>
+          <template #body="{ data }">
+            <button v-on:click="deleteRow(data.id)">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
             </svg>
